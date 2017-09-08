c0d3cd479b20aff2754c42cbdbc48b3f48a4b97e
wyb314 <13212777425@163.com>
2017-09-08 18:59:51 +0800

    Initial commit

---
+++ Assets/Scripts/Engine/Network/EchoConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Network
+{
+    class EchoConnection
+    {
+        protected struct ReceivedPacket
+        {
+            public int FromClient;
+            public byte[] Data;
+        }
+    }
+}
---
+++ Assets/Scripts/Engine/Network/OrderIO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Network
+{
+    public static class OrderIO
+    {
+        public static List<Order> ToOrderList(this byte[] bytes, World world)
+        {
+            var ms = new MemoryStream(bytes, 4, bytes.Length - 4);
+            var reader = new BinaryReader(ms);
+            var ret = new List<Order>();
+            while (ms.Position < ms.Length)
+            {
+                var o = Order.Deserialize(world, reader);
+                if (o != null)
+                    ret.Add(o);
+            }
+
+            return ret;
+        }
+    }
+}
